94f90d480604f37ba0b16caa9f75880954fc00c1
Nicolas Granata <nicolasegranata@gmail.com>
2025-11-19 19:40:46 -0300

    Testing dev/null ohmybash

--- bin/install-ohmybash.sh
+++ bin/install-ohmybash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Instalando Oh My Bash"
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" -s
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" </dev/null
